be1972aa954cffa4f0960d1b0517e78bfc27c35a
Suny Dutta <sanidutta6@gmail.com>
2023-05-05 20:35:31 +0530

    Added ME-1 Problem-19

--- Milestone-1 Exam/(19)Validate Password/JS/index.js
+++ Milestone-1 Exam/(19)Validate Password/JS/index.js
@@ -6,11 +6,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
+        const display = document.getElementById("show-status");
 
         const emailRegex = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi;
 
         if(emailRegex.test(email) == false || password.length < 8) {
-            console.log('Invalid email or password!');
+            display.innerText = 'Invalid email or password!';
+            display.style.color = "red";
+        } else {
+            display.innerText = 'Valid email and password!';
+            display.style.color = "green";
         }
     });
 });
