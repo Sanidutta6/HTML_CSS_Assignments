fb097ead483e2422260d3c37f618a438753edfd3
Suny Dutta <sanidutta6@gmail.com>
2023-05-21 17:52:43 +0530

    Updated ME-1 Problem-21

--- Milestone-1 Exam/(21)TODO App/JS/index.js
+++ Milestone-1 Exam/(21)TODO App/JS/index.js
@@ -9,49 +9,111 @@
 //     "tags": ""
 // }
 
-document.addEventListener("DOMContentLoaded", function() {
+function today() {
+    const date = new Date();
+    let dd = date.getDate();
+    let mm = date.getMonth() + 1;
+    let yyyy = date.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+
+    return `${dd}-${mm}-${yyyy}`;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
     let todoObjList = [];
+    let TODOCounter = 0;
     const editor = document.getElementById("new-todo-editor");
     const composeTodo = document.getElementById("compose-todo");
-    const closeEditor = document.getElementById("close");
+    const closeEditorBtn = document.getElementById("close");
     const addLabel = document.getElementById("add-label");
     const saveNewTodo = document.getElementById("save");
     // const saveNewTodoDraft = document.getElementById("save-draft");
     const deleteNewTodo = document.getElementById("delete");
+    const labelCreateBtn = document.getElementById("create-label");
+    const labelCloseBtn = document.getElementById("cancel");
+
+    // closes the editor upon calling.
+    function closeEditor() {
+        document.getElementById("todo-title").value = "";
+        document.getElementById("description").value = "";
+        editor.style.display = "none";
+    }
+
+    // closes the label window upon calling.
+    function closeLabelEditor() {
+        document.getElementById("label-name").value = "";
+        document.getElementById("new-label").style.display = "none";
+    }
 
     // Opens Editor for new Todo.
-    composeTodo.addEventListener("click", function() {
+    composeTodo.addEventListener("click", function () {
         editor.style.display = "block";
     });
 
     // Closes the editor
-    closeEditor.addEventListener("click", function() {
-        document.getElementById("todo-title").value = "";
-        document.getElementById("description").value = "";
-        editor.style.display = "none";
-    })
+    closeEditorBtn.addEventListener("click", closeEditor)
 
     // Adds New Label.
-    addLabel.addEventListener("click", function() {
-        console.log("ADD label Clicked");
+    addLabel.addEventListener("click", function () {
+        document.getElementById("new-label").style.display = "block";
     });
 
     // Save New Todo, if all fields are not empty.
-    saveNewTodo.addEventListener("click", function() {
+    saveNewTodo.addEventListener("click", function () {
         const title = document.getElementById("todo-title").value;
         const description = document.getElementById("description").value;
-        if(!(title && description)) {
+        if (!(title && description)) {
             console.log("Please Fill Up all the Fields");
             return;
         }
-        
+
+        // const date = new Date();
+        const newTodo = {
+            "title": title,
+            "description": description,
+            "date": today(),
+            "draft": false,
+            "checked": false,
+            "starred": false,
+            "tags": ""
+        }
+
+        todoObjList.push(newTodo);
+        TODOCounter++;
+        const todoElement = `
+        <div class="todo" data-id="${TODOCounter}">
+            <div class="checkbox" aria-checked="false">
+                <span class="checkmark"></span>
+            </div>
+            <div class="date">${newTodo["date"]}</div>
+            <div class="todo-title">${newTodo["title"]}</div>
+        </div>`;
+        document.getElementById("todo-items").innerHTML += todoElement;
+
+        closeEditor();
         console.log("Save TODO Clicked");
     });
 
     // Delete New Todo.
-    deleteNewTodo.addEventListener("click", function() {
-        document.getElementById("todo-title").value = "";
-        document.getElementById("description").value = "";
-        editor.style.display = "none";
-    });
+    deleteNewTodo.addEventListener("click", closeEditor);
+
+    // Closes the window for creating the label.
+    labelCloseBtn.addEventListener("click", closeLabelEditor);
+
+    // Saves new Label
+    labelCreateBtn.addEventListener("click", function () {
+        const newLabel = document.getElementById("label-name").value;
+        const labelElement = `
+                    <div class="label">
+                        <div class="label-color"></div>
+                        <div class="label-text">${newLabel}</div>
+                    </div>`;
+        document.getElementById("labels-list").innerHTML += labelElement;
+        closeLabelEditor();
+    })
 });
