9cb80ef4160a410820fe35caefc8d832ec82cfcf
Suny Dutta <sanidutta6@gmail.com>
2023-05-22 07:56:23 +0530

    Updated ME-1 Problem-1

--- Milestone-1 Exam/(1)Password Validator/JS/index.js
+++ Milestone-1 Exam/(1)Password Validator/JS/index.js
@@ -8,12 +8,19 @@ document.addEventListener('DOMContentLoaded', function() {
         let passwordStr = password.value;
         let confirmPasswordStr = confirmPassword.value;
 
+        if(!(passwordStr && confirmPasswordStr)) {
+            document.getElementsByClassName("show-validity")[0].textContent = "Please fill both password fields.";
+            return;
+        }
+
         if(passwordStr == confirmPasswordStr) {
             confirmPassword.style.boxShadow = "0 0 5pt 2pt green";
-            console.log("Password Matched. Password validation Successful.");
+            // console.log("Password Matched. Password validation Successful.");
+            document.getElementsByClassName("show-validity")[0].textContent = "Password Matched. Password validation Successful.";
         } else {
             confirmPassword.style.boxShadow = "0 0 5pt 2pt red";
-            console.log("Password didn't match. Password validation unsuccessful");
+            // console.log("Password didn't match. Password validation unsuccessful");
+            document.getElementsByClassName("show-validity")[0].textContent = "Password didn't match. Password validation unsuccessful.";
         }
     });
 });
