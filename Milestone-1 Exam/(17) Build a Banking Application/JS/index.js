bb70210bd81abf4fc3aaf659e9b087bcb3602111
Suny Dutta <sanidutta6@gmail.com>
2023-06-02 09:10:38 +0530

    Updated assignments

--- Milestone-1 Exam/(17) Build a Banking Application/JS/index.js
+++ Milestone-1 Exam/(17) Build a Banking Application/JS/index.js
@@ -1,15 +1,38 @@
+// const customerObj = {
+//     "account no": 12 digit integer,
+//     "name": String,
+//     "address": String,
+//     "balance": integer
+//     "transaction": [array of trasaction obj]
+// }
+
+// const transactionObj = {
+//     "time": "time",
+//     "type": "debit" || "credit",
+//     "amount": integer,
+// }
+
 document.addEventListener("DOMContentLoaded", function () {
     const dashboard = document.getElementById("dashboard");
     const controls = document.getElementById("controls");
-    const debitCredit = document.getElementById("debit-credit");
     const grievance = document.getElementById("grievance");
     const faq = document.getElementById("faq");
 
     const dashboardView = document.getElementById("dashboard-view");
     const controlsView = document.getElementById("controls-view");
-    const debitCreditView = document.getElementById("transaction-view");
     const grievanceView = document.getElementById("grievance-view");
     const faqView = document.getElementById("faq-view");
+
+    const addCustomer = document.getElementById("add-customer");
+    const removeCustomer = document.getElementById("remove-customer");
+    const showCustomer = document.getElementById("show-customer");
+    const debitCredit = document.getElementById("debit-credit");
+    const balanceInquiry = document.getElementById("balance-inquiry");
+    const transactionHistory = document.getElementById("transaction-history");
+
+    const onClickDisplay = document.getElementById("on-click-display");
+    const cancelDisplay = document.getElementById("cancel-display");
+    const displayHeaderText = document.querySelector("#display-header>h3");
 
     const accordionItems = document.querySelectorAll('.accordion-item');
     accordionItems.forEach(item => {
@@ -37,14 +60,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    debitCredit.addEventListener("click", function () {
-        if (prevView !== debitCreditView) {
-            prevView.style.display = "none";
-            debitCreditView.style.display = "block";
-            prevView = debitCreditView;
-        }
-    });
-
     grievance.addEventListener("click", function () {
         if (prevView !== grievanceView) {
             prevView.style.display = "none";
@@ -60,4 +75,52 @@ document.addEventListener("DOMContentLoaded", function () {
             prevView = faqView;
         }
     });
+
+    addCustomer.addEventListener("click", function() {
+        onClickDisplay.style.zIndex = "1";
+        displayHeaderText.innerText = "Add Customer";
+        document.getElementById("customer-display-template").style.display = "block";
+        console.log("Click");
+    });
+
+    removeCustomer.addEventListener("click", function() {
+        onClickDisplay.style.zIndex = "1";
+        displayHeaderText.innerText = "Remove Customer";
+        document.getElementById("customer-account-number-template").style.display = "block";
+        console.log("Click");
+    });
+
+    showCustomer.addEventListener("click", function() {
+        onClickDisplay.style.zIndex = "1";
+        displayHeaderText.innerText = "Show Customer";
+        document.getElementById("customer-account-number-template").style.display = "block";
+        console.log("Click");
+    });
+
+    debitCredit.addEventListener("click", function() {
+        onClickDisplay.style.zIndex = "1";
+        displayHeaderText.innerText = "Debit / Credit";
+        document.getElementById("customer-account-number-template").style.display = "block";
+        console.log("Click");
+    });
+
+    balanceInquiry.addEventListener("click", function() {
+        onClickDisplay.style.zIndex = "1";
+        displayHeaderText.innerText = "Balance Inquiry";
+        document.getElementById("customer-account-number-template").style.display = "block";
+        console.log("Click");
+    });
+
+    transactionHistory.addEventListener("click", function() {
+        onClickDisplay.style.zIndex = "1";
+        displayHeaderText.innerText = "Transaction History";
+        document.getElementById("customer-account-number-template").style.display = "block";
+        console.log("Click");
+    });
+
+    cancelDisplay.addEventListener("click", function() {
+        onClickDisplay.style.zIndex = "-1";
+        document.getElementById("customer-display-template").style.display = "none";
+        document.getElementById("customer-account-number-template").style.display = "none";
+    })
 });
